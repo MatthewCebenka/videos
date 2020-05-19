4709ea5612bf726e9b8f8142a910830a0eca3d95
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-19 14:48:37 -0400

    imported searchbar component into the app component

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
+import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Hi there!</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Hi there!</h1>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 export default App;
--- videos/src/components/SearchBar.js
+++ videos/src/components/SearchBar.js
@@ -1,1 +1,13 @@
-import React from 'react';
+import React from "react";
+
+class SearchBar extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hi there from the search bar component!</h1>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
