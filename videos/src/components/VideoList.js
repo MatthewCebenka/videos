b49dd51d81b41a76f5a779541d920c63b874c4eb
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-20 11:59:50 -0400

    imported the videoItem component into the VideoList component

--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
+import VideoItem from './VideoItem';
 
 const VideoList = (props) => {
     return(
         <div> 
             <h1>I have {props.videos.length} videos.</h1>
-            
+            <VideoItem />
         </div>
     )
 }
