b9417d04756d4a6369436413b542eb63275ee4d4
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-20 14:26:41 -0400

    passed the onvideoselect function prop from the videolist component to the videoItem component

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = (props) => {
   const title = props.video.snippet.title;
   const image = props.video.snippet.thumbnails.medium.url;
   return (
-    <div className="video-item item">
+    <div className="video-item item" onClick={(e) => {props.onVideoSelect(e)}}>
       <img className="ui image" src={image} />
       <div className='content'>
         <div className='header' >{title}</div>
