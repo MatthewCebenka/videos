cc9d7953ece117a46803084c63f399c7d52512b3
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-20 14:57:16 -0400

    got the onVideoSelect function to pass the callback function properly

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -5,7 +5,7 @@ const VideoItem = (props) => {
   const title = props.video.snippet.title;
   const image = props.video.snippet.thumbnails.medium.url;
   return (
-    <div className="video-item item" onClick={(e) => {props.onVideoSelect(e)}}>
+    <div className="video-item item" onClick={() => {props.onVideoSelect(props.video)}}>
       <img className="ui image" src={image} />
       <div className='content'>
         <div className='header' >{title}</div>
