db9bf8072e12987aedf8d7ace84abb65dd0236b3
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-20 09:17:13 -0400

    made an axios.create instance in the youtube component

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
+
 const KEY = 'AIzaSyApX_Ug1NI714qFYx4CMFatV4cQUV3j9lk';
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: KEY
+    }
+});
