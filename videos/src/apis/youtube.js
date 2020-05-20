68cece512bd034d64231efd58a989fc693a3e9cb
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-20 11:29:30 -0400

    hid api key and changed the query params location to account for the axios bug

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
-
-const KEY = 'AIzaSyApX_Ug1NI714qFYx4CMFatV4cQUV3j9lk';
-
 export default axios.create({
-    baseURL: 'https://www.googleapis.com/youtube/v3',
-    params: {
-        part: 'snippet',
-        type: 'video',
-        maxResults: 5,
-        key: KEY
-    }
+    baseURL: 'https://www.googleapis.com/youtube/v3'
 });
--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,11 +1,20 @@
 import React from "react";
 import SearchBar from "./SearchBar";
+import youtube from '../apis/youtube'
 
 class App extends React.Component {
     
     onTermSubmit = (searchTerm) => {
-        
-    }
+        const API_KEY = process.env.REACT_APP_GOOGLE_API_KEY;
+        youtube.get('/search', {
+            params: {
+                q: searchTerm,
+                part: 'snippet',
+                maxResults: 5,
+                key: API_KEY
+            }
+        });
+    };
 
 
   render() {
