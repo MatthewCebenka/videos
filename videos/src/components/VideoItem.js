f563ff18cb2927d2fb7be68345543d962c9a21e3
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-20 12:25:24 -0400

    rendered the titles and video thumbnail

--- videos/src/components/VideoItem.js
+++ videos/src/components/VideoItem.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 const VideoItem = (props) => {
     const title = props.video.snippet.title;
+    const image = props.video.snippet.thumbnails.medium.url;
     return (
         <div>
             <h1>{title}</h1>
+            <img src={image}/>
         </div>
     )
 }
