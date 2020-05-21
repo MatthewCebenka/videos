589c40f8beb2ac3d5a40e067d2d0c35c361b8447
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-21 12:27:48 -0400

    removed loader spinner

--- videos/src/components/VideoDetail.js
+++ videos/src/components/VideoDetail.js
@@ -1,6 +1,5 @@
 import React from "react";
 import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-import Loader from "react-loader-spinner";
 
 const VideoDetail = ({ video }) => {
   if (!video) {
