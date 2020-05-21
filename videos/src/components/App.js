fb40d3e0504986a6196554dd400f2fac5cd72d04
Matthew Cebenka <matthewt.cebenka@gmail.com>
2020-05-21 13:05:56 -0400

    changed mounting search

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
   state = { videos: [], selectedVideo: null };
 
     componentDidMount(){
-        this.onTermSubmit('trending')
+        this.onTermSubmit('news')
     }
 
   onTermSubmit = async (searchTerm) => {
